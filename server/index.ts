c2497f43fdbe790a6657178db340e6015f3447cc
Asirwad <asirwadsali@gmail.com>
2025-02-05 12:16:41 +0530

    Connected to MongoDB

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import "dotenv/config";
 import http from 'http';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 
 const app = express();
@@ -10,7 +11,12 @@ const {PORT} = process.env || 4000;
 
 app.use(cors());
 
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+mongoose.connect(process.env.MONGO_URL!);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
